81cefb07e62f8b69a9cbaa90dd767e3d33520cc5
icew4ll <icew4ll@tuta.io>
2020-05-01 07:29:21 -0700

    2020-05-01 14:28:48.405650941 UTC

--- src/components/Pyth.tsx
+++ src/components/Pyth.tsx
@@ -1,19 +1,32 @@
 import React, { FC, useState, ChangeEvent } from 'react'
-import { InlineMath, BlockMath } from 'react-katex'
+import { InlineMath } from 'react-katex'
 import Svg from './Svg'
 
 interface Props {}
 
 export const Pyth: FC<Props> = () => {
-  const [b, setValue] = useState<string>('')
-  const [b2, set2] = useState<number>(0)
+  const [b, setB] = useState<string>('')
+  const [height, setHeight] = useState<string>('')
+  const [hiddenHeight, setHH] = useState<number>(0)
+  const [distanceHorizon, setDH] = useState<number>(0)
+  //const radius = 3963
+  const radius = 3958.756
+  const feet = 5280
+
+  function handleHeight(e: ChangeEvent<HTMLInputElement>) {
+    // numbers only input
+    const height = e.target.value.replace(/\D/, '')
+    setHeight(height)
+    setDH(Math.sqrt((radius + parseInt(height) / 5280) ** 2 - radius ** 2))
+    //setDH(Math.sqrt((radius + parseInt(height) * 5280) ** 2 - radius ** 2))
+  }
 
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     // numbers only input
-    const radius = 3963
-    const feet = 5280
-    setValue(e.target.value.replace(/\D/, ''))
-    set2((Math.sqrt(parseInt(e.target.value.replace(/\D/, '')) ** 2 + radius ** 2) - radius) * feet)
+    const td = e.target.value.replace(/\D/, '')
+    const b = parseInt(td) - distanceHorizon
+    setB(td)
+    setHH((Math.sqrt(b ** 2 + radius ** 2) - radius) * feet)
   }
 
   return (
@@ -24,7 +37,7 @@ export const Pyth: FC<Props> = () => {
         <Svg />
         {/* comment */}
         <div className="flex-1 px-2 bg-gray-900">
-          <ul className="text-md list-none">
+          <ul>
             <li>
               <InlineMath>{'a^{2} + b^{2} = c^{2}'}</InlineMath>
             </li>
@@ -32,14 +45,26 @@ export const Pyth: FC<Props> = () => {
               <InlineMath>{'a^{2} + b^{2} = (a + h)^{2}'}</InlineMath>
             </li>
             <li>
+              <InlineMath>{'b = \\sqrt{(a + h)^{2} - a^{2}}'}</InlineMath>
+            </li>
+            <li>Height (feet) = {height}</li>
+            <li>
+              <input className="text-gray-900" value={height} onChange={handleHeight} />
+            </li>
+            <li>b (miles) = {distanceHorizon}</li>
+          </ul>
+          <br />
+          <ul className="text-md list-none">
+            <li>Hidden height</li>
+            <li>
               <InlineMath>{'h = \\sqrt{a^{2} + b^{2}} - a'}</InlineMath>
             </li>
-            <li>a (miles) = 3963</li>
+            <li>a (miles) = {radius}</li>
             <li>b (miles) = {b}</li>
             <li>
               <input className="text-gray-900" value={b} onChange={handleChange} />
             </li>
-            <li>h = {b2} hidden height (feet)</li>
+            <li>h = {hiddenHeight} hidden height (feet)</li>
           </ul>
         </div>
       </div>
