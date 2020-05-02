938fb92fc6ed83693029b77095c49a6c86082d5f
icew4ll <icew4ll@tuta.io>
2020-05-02 07:16:37 -0700

    2020-05-02 14:16:13.727408394 UTC

--- src/components/Pyth.tsx
+++ src/components/Pyth.tsx
@@ -9,35 +9,49 @@ export const Pyth: FC<Props> = () => {
   const [height, setHeight] = useState<string>('')
   const [hiddenHeight, setHH] = useState<number>(0)
   const [distanceHorizon, setDH] = useState<number>(0)
+  const [inputs, setInputs] = useState({ string })
   //const radius = 3963
   const radius = 3958.756
   const feet = 5280
 
-  function handleHeight(e: ChangeEvent<HTMLInputElement>) {
+  const handleHeight = (e: ChangeEvent<HTMLInputElement>) => {
     // numbers only input
     const height = e.target.value.replace(/\D/, '')
-    setHeight(height)
-    setDH(Math.sqrt((radius + parseInt(height) / 5280) ** 2 - radius ** 2))
-    //setDH(Math.sqrt((radius + parseInt(height) * 5280) ** 2 - radius ** 2))
+    calcDH(height)
   }
 
-  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+  const calcDH = (height: string) => {
+    setHeight(height)
+    setDH(Math.sqrt((radius + parseInt(height) / 5280) ** 2 - radius ** 2))
+    console.log(height, hiddenHeight)
+  }
+
+  const handleHidden = (e: ChangeEvent<HTMLInputElement>) => {
     // numbers only input
     const td = e.target.value.replace(/\D/, '')
     const b = parseInt(td) - distanceHorizon
     setB(td)
+    calcHH(b)
+  }
+
+  const calcHH = (b: any) => {
     setHH((Math.sqrt(b ** 2 + radius ** 2) - radius) * feet)
+  }
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.persist()
+    setInputs((inputs) => ({ ...inputs, [e.target.name]: e.target.value }))
+    console.log(inputs)
   }
 
   return (
     <div>
       <h3 className="text-gray-500">Pythagorean theorum:</h3>
-
       <div className="flex flex-wrap">
         <Svg />
         {/* comment */}
-        <div className="flex-1 px-2 bg-gray-900">
+        <div className="flex-1 px-2 bg-gray-800">
           <ul>
+            <li className="underline">Part 1: Horizon Distance</li>
             <li>
               <InlineMath>{'a^{2} + b^{2} = c^{2}'}</InlineMath>
             </li>
@@ -47,29 +61,35 @@ export const Pyth: FC<Props> = () => {
             <li>
               <InlineMath>{'b = \\sqrt{(a + h)^{2} - a^{2}}'}</InlineMath>
             </li>
-            <li>Height (feet) = {height}</li>
+            <li>Observer Height (feet) = {height}</li>
             <li>
               <input className="text-gray-900" value={height} onChange={handleHeight} />
             </li>
-            <li>b (miles) = {distanceHorizon}</li>
+            <li>Distance from Observer to Horizon = {distanceHorizon} miles</li>
           </ul>
           <br />
           <ul className="text-md list-none">
-            <li>Hidden height</li>
+            <li className="underline">Part 2: Hidden Distance</li>
+            <li>
+              <InlineMath>{'a^{2} + b^{2} = c^{2}'}</InlineMath>
+            </li>
+            <li>
+              <InlineMath>{'a^{2} + b^{2} = (a + h)^{2}'}</InlineMath>
+            </li>
             <li>
               <InlineMath>{'h = \\sqrt{a^{2} + b^{2}} - a'}</InlineMath>
             </li>
-            <li>a (miles) = {radius}</li>
-            <li>b (miles) = {b}</li>
+            <li>Alleged Earth Radius = {radius} miles</li>
+            <li>Distance from Observer to Target = {b} miles</li>
             <li>
-              <input className="text-gray-900" value={b} onChange={handleChange} />
+              <input name="hidden" className="text-gray-900" onChange={handleInputChange} />
             </li>
-            <li>h = {hiddenHeight} hidden height (feet)</li>
+            <li>Hidden Height = {hiddenHeight} feet</li>
           </ul>
         </div>
       </div>
     </div>
   )
 }
-
+//https://medium.com/@geeky_writer_/using-react-hooks-to-create-awesome-forms-6f846a4ce57
 export default Pyth
--- src/components/Svg.tsx
+++ src/components/Svg.tsx
@@ -1,6 +1,7 @@
 export default function ClassName() {
+  const cy = 130
   return (
-    <svg height="100%" width="100%" viewBox="0 0 200 200" className="w-48 p-2 bg-gray-900">
+    <svg height="100%" width="100%" viewBox="0 0 200 240" className="max-w-xs p-2 bg-gray-900">
       <defs>
         <linearGradient id="myGradient" gradientTransform="rotate(90)">
           <stop offset="5%" stopColor="#3785de" />
@@ -8,28 +9,51 @@ export default function ClassName() {
         </linearGradient>
       </defs>
       {/*min-x, min-y, width and height*/}
-      <circle cx={100} cy={100} r={100} fill="url(#myGradient)" />
-      <text fill="white" x="80" y="50" className="text-xl">
+      <circle cx={100} cy={140} r={100} fill="url(#myGradient)" />
+      <text fill="white" x="34" y="10" className="text-xs">
+        Part 1:
+      </text>
+      <text fill="white" x="4" y="25" className="text-xs">
+        Horizon Distance
+      </text>
+      <text fill="white" x="135" y="10" className="text-xs">
+        Part 2:
+      </text>
+      <text fill="white" x="108" y="25" className="text-xs">
+        Hidden Distance
+      </text>
+      <text fill="white" x="85" y="80" className="text-xl">
         a
       </text>
-      <text fill="white" x="140" y="20" className="text-xl">
+      <text fill="white" x="138" y="58" className="text-xl">
         b
       </text>
-      <text fill="white" x="155" y="65" className="text-xl">
+      <text fill="white" x="50" y="58" className="text-xl">
+        b
+      </text>
+      <text fill="white" x="50" y="85" className="text-xl">
         c
       </text>
-      <text fill="white" x="187" y="32" className="text-xl">
+      <text fill="white" x="138" y="85" className="text-xl">
+        c
+      </text>
+      <text fill="white" x="187" y="72" className="text-xl">
+        h
+      </text>
+      <text fill="white" x="5" y="72" className="text-xl">
         h
       </text>
       {/* LINES */}
       {/* (x2, y2), (x1, y1) */}
 
       {/* b */}
-      <path strokeWidth="2" stroke="red" d="M 200 1 L 100 1" />
+      <path strokeWidth="2" stroke="red" d="M 200 40 L 100 40" />
+      <path strokeWidth="2" stroke="red" d="M 0 40 L 100 40" />
       {/* a */}
-      <path strokeWidth="2" stroke="red" d="M 100 0 L 100 100" />
+      <path strokeWidth="2" stroke="red" d="M 100 40 L 100 140" />
       {/* c */}
-      <path strokeWidth="2" stroke="red" d="M 200 0 L 100 100" />
+      <path strokeWidth="2" stroke="red" d="M 200 40 L 100 140" />
+      <path strokeWidth="2" stroke="red" d="M 0 40 L 100 140" />
     </svg>
   )
 }
